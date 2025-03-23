c3c1702c6fd00e6e23b1ce9e4fc94d09ed6d8999
lulli <chupachups0969@gmail.com>
2025-03-23 22:39:16 +0800

    hides the api keys

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,16 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyByPEaxcA_z_qtNBJq7wvXLQx2DIWfNRTg",
-  authDomain: "gym-management-system-7a77a.firebaseapp.com",
-  projectId: "gym-management-system-7a77a",
-  storageBucket: "gym-management-system-7a77a.appspot.com",
-  messagingSenderId: "305036963651",
-  appId: "1:305036963651:web:df7000f2bb7e22514889f6",
-  measurementId: "G-9RF8LQPW2K",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
